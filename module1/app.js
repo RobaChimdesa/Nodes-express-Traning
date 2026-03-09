9c206cb6ecaf75e90c32b479c5ec17ac076d3f1e
Roba Chimdesa <robachimdesa123@gmail.com>
2026-03-09 14:38:33 +0300

    old method of js import and export practice

--- module1/app.js
+++ module1/app.js
@@ -6,8 +6,13 @@ const result = math.sum(5, 10);
 const subt = math.sub(10, 5);
 const mul = math.mul(5, 10);
 const div = math.div(10, 5);
+const rem = math.remainder(10, 3);
+const even = math.isEven(9);
+
 
 console.log("The sum of 5 and 10 is: " + result);
 console.log("The subtraction of 10 and 5 is: " + subt);
 console.log("The multiplication of 5 and 10 is: " + mul);
 console.log("The division of 10 and 5 is: " + div);
+console.log("The remainder of 10 and 3 is: " + rem);
+console.log("even or odd: " + even);
--- module1/mathfunc.js
+++ module1/mathfunc.js
@@ -1,24 +1,41 @@
 // Adding Two numbers
-function sum(a, b) 
-{
+function sum(a, b) {
   return a + b;
 }
 
-function sub(a, b)
-{
-    return a - b;
+function sub(a, b) {
+  return a - b;
 }
-function mul(a, b)
-{
-    return a * b;
+function mul(a, b) {
+  return a * b;
 }
-function div(a,b){
-    if(b === 0){
-        return "Division by zero is not allowed.";
-    }
-    return a / b;
+function div(a, b) {
+  if (b === 0) {
+    return "Division by zero is not allowed.";
+  }
+  return a / b;
 }
 // reminder ->
+function remainder(a, b) {
+  if (b === 0) {
+    return "Division by zero is not allowed.";
+  }
+  return a % b;
+}
 // even odd  -> use reminder dunc
 
-module.exports = { sum, sub, mul, div };
+// function isEven(remainder) {
+//   if (remainder === 0) {
+//     return "The number is even.";
+//   }
+// }
+
+function isEven(a){
+    if (a % 2 === 0) {
+        return "The number is even.";}
+    else {
+        return "The number is odd.";
+    }
+}
+
+module.exports = { sum, sub, mul, div, remainder, isEven };
