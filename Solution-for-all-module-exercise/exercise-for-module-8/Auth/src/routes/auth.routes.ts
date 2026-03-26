8849d38253e22234f43bd87e32127b661196f2e6
RobaChimdesa <robachimdesa123@gmail.com>
2026-03-26 15:45:38 +0300

    feat: add rate limiter middleware for authentication routes

--- Solution-for-all-module-exercise/exercise-for-module-8/Auth/src/routes/auth.routes.ts
+++ Solution-for-all-module-exercise/exercise-for-module-8/Auth/src/routes/auth.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { register, login, refreshToken } from '../controllers/auth.controller';
-
+import { authRateLimiter } from '../middleware/rateLimiter';
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', authRateLimiter,  register);
+router.post('/login',  authRateLimiter, login);
 router.post('/refresh', refreshToken)
 
 export default router;
